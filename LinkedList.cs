657bc85900e6ac22aeedce26542b672ea71fea72
Saikumar <muthyalasaikumar9@gmail.com>
2022-06-01 20:38:05 +0530

    commit

---
+++ LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        //public void Add(int data)
+        //{
+        //    Node n = new Node(data);
+        //    if (head == null)
+        //    {
+        //        head = n;
+        //        Console.WriteLine("{0} inserted into the linkedList\n", n.data);
+        //    }
+        //    else
+        //    {
+        //        n.next = head;
+        //        head = n;
+        //        Console.WriteLine("{0} inserted into the linkedlist\n", n.data);
+        //    }
+        //}
+        public void Append(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                Console.WriteLine("{0} Appended", n.data);
+            }
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = n;
+            Console.WriteLine("{0} Appended", n.data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying the LinkedList elements\n");
+            Node t = head;
+            if (t == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.data + " ");
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------WELCOME TO THE LINKEDLIST  PROGRAM--------------------------\n\n");
+            LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Display();
+        }
+    }
+}
